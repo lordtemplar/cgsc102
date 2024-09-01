7f201971702aa5dcd606ee3b65873ff7dc40abb1
lordtemplar <lordtemplar0@gmail.com>
2024-09-01 19:03:12 +0700

    Update studentscreen.py

--- studentscreen.py
+++ studentscreen.py
@@ -15,14 +15,14 @@ st.set_page_config(page_title="LIVE Position")
 # ฟังก์ชันในการดึงข้อมูลและแสดงผล
 def load_data_and_render_table():
     # เปิดไฟล์ Google Sheets และดึงข้อมูล
-    position_sheet = client.open_by_url('https://docs.google.com/spreadsheets/d/1mflUv6jyOqTXplPGiSxCOp7wJ1HHd4lQ4BSIzvuBgoQ').sheet1
+    position_sheet = client.open_by_url('https://docs.google.com/spreadsheets/d/1iKu8mpZeDXonDQhX-mJtDWx_-g68TSGlefdCXdle8ec/edit?usp=sharing').sheet1
     df_positions = pd.DataFrame(position_sheet.get_all_records())
 
     # ปรับ ID เป็นเลข 3 ตำแหน่ง
     df_positions['PositionID'] = df_positions['PositionID'].apply(lambda x: f"{int(x):03d}")
 
     # เพิ่มกล่องค้นหาด้านบน
-    search_term = st.text_input("ค้นหา รหัสตำแหน่ง, ชื่อตำแหน่ง หรือ ชกท. (Specialist)")
+    search_term = st.text_input("ค้นหา ลำดับ, ตำแหน่ง, สังกัด, ชกท., อัตรา, เหล่า หรือ เงื่อนไข")
 
     def get_bg_color(status):
         """ฟังก์ชันเพื่อคืนค่าสีพื้นหลังตามสถานะ"""
@@ -34,10 +34,10 @@ def load_data_and_render_table():
     def render_simple_table(data):
         """ฟังก์ชันในการสร้างและแสดงผลตารางแบบง่าย"""
         html_table = '<table style="width:100%;">'
-        html_table += '<tr><th>รหัสตำแหน่ง</th><th>ชื่อตำแหน่ง</th><th>ชกท.</th></tr>'
+        html_table += '<tr><th>ลำดับ</th><th>ตำแหน่ง</th><th>สังกัด</th><th>ชกท.</th><th>อัตรา</th><th>เหล่า</th><th>เงื่อนไข</th></tr>'
         for _, row in data.iterrows():
             bg_color = get_bg_color(row['Status'])
-            html_table += f'<tr style="background-color:{bg_color}; color:white;"><td>{row["PositionID"]}</td><td>{row["PositionName"]}</td><td>{row["Specialist"]}</td></tr>'
+            html_table += f'<tr style="background-color:{bg_color}; color:white;"><td>{row["PositionID"]}</td><td>{row["PositionName"]}</td><td>{row["Unit"]}</td><td>{row["Specialist"]}</td><td>{row["Rank"]}</td><td>{row["Branch"]}</td><td>{row["Other"]}</td></tr>'
         html_table += '</table>'
 
         # แสดงผลตาราง
@@ -45,7 +45,7 @@ def load_data_and_render_table():
 
     # กรองข้อมูลตามคำค้นหา
     if search_term:
-        filtered_positions = df_positions[df_positions.apply(lambda row: search_term.lower() in row['PositionID'].lower() or search_term.lower() in row['PositionName'].lower() or search_term.lower() in row['Specialist'].lower(), axis=1)]
+        filtered_positions = df_positions[df_positions.apply(lambda row: search_term.lower() in row['PositionID'].lower() or search_term.lower() in row['PositionName'].lower() or search_term.lower() in row['Unit'].lower() or search_term.lower() in row['Specialist'].lower() or search_term.lower() in row['Rank'].lower() or search_term.lower() in row['Branch'].lower() or search_term.lower() in row['Other'].lower(), axis=1)]
     else:
         filtered_positions = df_positions
 
