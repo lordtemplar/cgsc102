d7f645f9a655bc3a4322d90e65630a13140f0178
lordtemplar <lordtemplar0@gmail.com>
2024-09-07 11:42:45 +0700

    Update position_recheck_firebase.py

--- position_recheck_firebase.py
+++ position_recheck_firebase.py
@@ -6,17 +6,17 @@ import pandas as pd
 
 # Load Firebase credentials from secrets
 firebase_config = {
-    "type": st.secrets["firebase"]["type"],
-    "project_id": st.secrets["firebase"]["project_id"],
-    "private_key_id": st.secrets["firebase"]["private_key_id"],
-    "private_key": st.secrets["firebase"]["private_key"].replace('\\n', '\n'),
-    "client_email": st.secrets["firebase"]["client_email"],
-    "client_id": st.secrets["firebase"]["client_id"],
-    "auth_uri": st.secrets["firebase"]["auth_uri"],
-    "token_uri": st.secrets["firebase"]["token_uri"],
-    "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
-    "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"],
-    "universe_domain": st.secrets["firebase"]["universe_domain"]
+    "type": st.secrets["firebase1"]["type"],
+    "project_id": st.secrets["firebase1"]["project_id"],
+    "private_key_id": st.secrets["firebase1"]["private_key_id"],
+    "private_key": st.secrets["firebase1"]["private_key"].replace('\\n', '\n'),
+    "client_email": st.secrets["firebase1"]["client_email"],
+    "client_id": st.secrets["firebase1"]["client_id"],
+    "auth_uri": st.secrets["firebase1"]["auth_uri"],
+    "token_uri": st.secrets["firebase1"]["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["firebase1"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["firebase1"]["client_x509_cert_url"],
+    "universe_domain": st.secrets["firebase1"]["universe_domain"]
 }
 
 # Initialize Firebase Admin SDK only once
