671eeee466474f8a55f7dc3903188d4e73056fa3
lordtemplar <lordtemplar0@gmail.com>
2024-09-09 22:46:46 +0700

    Update position_confirm_firebase.py

--- position_confirm_firebase.py
+++ position_confirm_firebase.py
@@ -192,7 +192,7 @@ if rank_query:
 
                 # Send Line Notify with the new token
                 next_rank = int(student_info['Rank']) + 1
-                line_token = "IXXqxz9o2oBAzUKPm38tLqijrzs91zoT51M6D7zCINj"
+                line_token = "bAypySAW0B5cMfPY2y4kEX2GJV9i9R5B2O4Mz48LdEG"
                 message = f"ลำดับที่ {student_info['Rank']}, รหัสนักเรียน {student_info['StudentID']}, {st.session_state['rank_name']}, เลือกรับราชการในตำแหน่ง {matching_positions[selected_position_id]} ต่อไปเชิญลำดับที่ {next_rank} เลือกที่ลงต่อครับ"
                 send_line_notify(message, line_token)
 
